f4eb43419cb2fdb23a13c65cabe15cb0c26c0a24
Varun Jain <jainvarun@vmware.com>
2020-10-19 11:54:52 -0700

    Introduce ContentSourceBinding CRD
    This patch introduces ContentSourceBinding CRD that binds a
    ContentSource to a supervisor namespace. An instance of this CRD
    in a supervisor namespace indicates that the namespace has access
    to the ContentSource it points to.

---
+++ api/v1alpha1/contentsourcebinding_types.go
@@ -0,0 +1,45 @@
+// Copyright (c) 2020 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ContentSourceReference contains info to locate a Kind ContentSource object
+type ContentSourceReference struct {
+	// API version of the referent.
+	APIVersion string `json:"apiVersion,omitempty"`
+	// Kind is the type of resource being referenced.
+	Kind string `json:"kind,omitempty"`
+	// Name is the name of resource being referenced.
+	Name string `json:"name"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:scope=Namespaced,shortName=contentsourcebinding
+// +kubebuilder:printcolumn:name="ContentSource",type="string",JSONPath=".contentSourceRef.name"
+
+// ContentSourceBinding is a binding object responsible for
+// defining a ContentSource and a Namespace associated with it
+type ContentSourceBinding struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// ContentSourceRef is a reference to a ContentSource object
+	ContentSourceRef ContentSourceReference `json:"classRef,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ContentSourceBindingList contains a list of ContentSourceBinding
+type ContentSourceBindingList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ContentSourceBinding `json:"items"`
+}
+
+func init() {
+	RegisterTypeWithScheme(&ContentSourceBinding{}, &ContentSourceBindingList{})
+}
